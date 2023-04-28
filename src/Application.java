3cf6528fd87cce7e9c0afd4a92b2eff382e212ab
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-04-28 15:45:28 +0200

    started Application

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,78 @@
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Scanner;
+
 public class Application {
+    Scanner scanner = new Scanner(System.in);
+    DbHandler dbHandler = new DbHandler();
+
+    public void start(){
+        boolean run = true;
+        Menu.mainMenu();
+        int choice = Integer.parseInt(scanner.nextLine());
+        while (run){
+            switch(choice){
+                case 1:
+                    addCustomer();
+                    break;
+                case 2:
+                    addEmployee();
+                    break;
+                case 3:
+                    deleteCustomer();
+                    break;
+                case 4:
+                    deleteEmployees();
+                    break;
+                case 5:
+                    updateCustomer();
+                    break;
+                case 6:
+                    updateEmployees();
+                    break;
+                case 7:
+                    listAllCustomers();
+                    break;
+                case 8:
+                    listAllEmployees();
+                    break;
+                case 9:
+                    Menu.exit();
+                    run = false;
+                    break;
+            }
+        }
+    }
+
+    private void listAllEmployees() {
+        MongoFacade.listAllEmployees();
+    }
+
+    private void listAllCustomers() {
+        MongoFacade.listAllCustomers();
+    }
+
+    private void updateEmployees() {
+        MongoFacade.updateEmployees();
+    }
+
+    private void updateCustomer() {
+        MongoFacade.updateCustomer();
+
+    }
+
+    private void deleteEmployees() {
+        MongoFacade.deleteEmployees();
+    }
+
+    private void deleteCustomer() {
+        MongoFacade.deleteCustomer();
+    }
+
+    private void addEmployee() {
+        MongoFacade.addEmployee();
+    }
+
+    private void addCustomer() {
+        MongoFacade.addCustomer();
+    }
 }
--- src/DbHandler.java
+++ src/DbHandler.java
@@ -1,19 +1,28 @@
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
 
+import com.mongodb.*;
+import com.mongodb.client.*;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.List;
 
 public class DbHandler {
 
     Keyreader keyreader = new Keyreader("Pass");
     String connectionString = "mongodb+srv://emilsivertsson:"+keyreader.getPasscode()+"@homecluster.ekna2wp.mongodb.net/?retryWrites=true&w=majority";
 
+    String DB = "Person";
+
+
+
+
+
 
 }
+
 
 
 
